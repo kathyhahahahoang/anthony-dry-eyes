7180398b9d93331842d33cd79be085cb96f68e27
Kathy Hoang <kathyhahoang@gmail.com>
2024-03-18 11:17:33 -0500

    Create AppLayout

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,22 @@
+import { Outlet, ScrollRestoration, useNavigation } from "react-router-dom";
+import styles from "./AppLayout.module.scss";
+import NavBar from "./NavBar";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className={styles.container}>
+      <ScrollRestoration />
+      {isLoading && <LoadingSpinner />}
+      <NavBar />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default AppLayout;
