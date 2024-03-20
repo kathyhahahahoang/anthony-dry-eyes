270345526df62aedd790e197e1f5d29e80017182
Kathy Hoang <kathyhahoang@gmail.com>
2024-03-20 12:03:31 -0500

    Change hero layout

--- src/ui/home/Hero.tsx
+++ src/ui/home/Hero.tsx
@@ -1,37 +1,20 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Hero.module.scss";
-import felix from "../../assets/img/felix.jpg";
-import { RiCheckFill } from "@remixicon/react";
+import desert from "../../assets/img/desert.jpg";
 
 function Hero() {
   return (
     <div className={styles.container}>
-      <div className={styles["hero-container"]}>
-        <div className={styles["hero-contents"]}>
-          <div className={styles["hero-text"]}>
-            <div className={styles["hero-text-contents"]}>
-              <h1>Are dry eyes affecting your quality of life?</h1>
-              <h3>We can help you find solutions to improve your eye health</h3>
-              <ul className={styles["list-container"]}>
-                <li className={styles["list-item"]}>
-                  <RiCheckFill />
-                  <p>Reason 1</p>
-                </li>
-                <li className={styles["list-item"]}>
-                  <RiCheckFill />
-                  <p>Reason 2</p>
-                </li>
-                <li className={styles["list-item"]}>
-                  <RiCheckFill />
-                  <p>Reason 3</p>
-                </li>
-              </ul>
-              <button className={styles.button}>Click</button>
-            </div>
-          </div>
-          <div className={styles["hero-picture"]}>
-            <img src={felix} />
-          </div>
-        </div>
+      <img src={desert} className={styles.hero} />
+      <div className={styles.box}>
+        <h1 className={styles.title}>Is that better?</h1>
+        <p className={styles.description}>
+          We know that dry eyes can negatively affect your quality of life.
+          That's why we're here to help you
+        </p>
+        <NavLink to="/blog" className={styles.button}>
+          Click
+        </NavLink>
       </div>
     </div>
   );
