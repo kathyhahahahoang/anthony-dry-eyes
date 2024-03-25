d3008d9e53794a4125c9851580a19cc64a03b0aa
Kathy Hoang <kathyhahoang@gmail.com>
2024-03-25 11:28:09 -0500

    Update Footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,20 +7,44 @@ function Footer() {
       <NavLink to="/" className={styles.logo}>
         Anthony
       </NavLink>
-      <ul className={styles["content-container"]}>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/blog">Blog</NavLink>
-        </li>
-        <li>
-          <NavLink to="/tips">Tips</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
+      <div className={styles["content-container"]}>
+        <ul className={styles["links-container"]}>
+          <li>
+            <NavLink to="/about">
+              <p className={styles["footer-title"]}>About</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog">
+              <p className={styles["footer-title"]}>Blog</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/tips">
+              <p className={styles["footer-title"]}>Tips</p>
+            </NavLink>
+          </li>
+          <li>
+            <p className={styles["footer-title"]}>Hours</p>
+            <div className={styles["info-details"]}>
+              <p>Monday - Friday: 9 - 5pm</p>
+              <p>Weekends: closed</p>
+            </div>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <p className={styles["footer-title"]}>Contact</p>
+            </NavLink>
+            <div className={styles["info-details"]}>
+              <p>Practice Name</p>
+              <p>1234 Address St.</p>
+              <p>Fakesville McFakesTown USA</p>
+              <p className={styles.telephone}>+1 234 567 8910</p>
+              <p className={styles.telephone}>email@email.com</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
