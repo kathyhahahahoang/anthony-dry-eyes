fff52f732d3bd5e11702f72988ecfe7d19620ee7
Kathy Hoang <kathyhahoang@gmail.com>
2024-03-28 11:50:37 -0500

    Create media queries for BlogSection

--- src/ui/home/BlogSection.tsx
+++ src/ui/home/BlogSection.tsx
@@ -11,23 +11,31 @@ function BlogSection() {
       <div className={styles["articles-container"]}>
         <div className={styles.article}>
           <img src={felix4} className={styles.picture} />
-          <p className={styles.date}>March 9, 2024</p>
-          <h3>How you can improve dry eyes</h3>
+          <div className={styles["text-container"]}>
+            <p className={styles.date}>March 9, 2024</p>
+            <h3>How you can improve dry eyes</h3>
+          </div>
         </div>
         <div className={styles.article}>
           <img src={felix2} className={styles.picture} />
-          <p className={styles.date}>March 9, 2024</p>
-          <h3>Signs to look out for</h3>
+          <div className={styles["text-container"]}>
+            <p className={styles.date}>March 9, 2024</p>
+            <h3>Signs to look out for</h3>
+          </div>
         </div>
         <div className={styles.article}>
           <img src={felix3} className={styles.picture} />
-          <p className={styles.date}>March 9, 2024</p>
-          <h3>Foods that can help improve your dry eyes</h3>
+          <div className={styles["text-container"]}>
+            <p className={styles.date}>March 9, 2024</p>
+            <h3>Foods that can help improve your dry eyes</h3>
+          </div>
         </div>
         <div className={styles.article}>
           <img src={felix5} className={styles.picture} />
-          <p className={styles.date}>March 9, 2024</p>
-          <h3>Are dry eyes genetic?</h3>
+          <div className={styles["text-container"]}>
+            <p className={styles.date}>March 9, 2024</p>
+            <h3>Are dry eyes genetic?</h3>
+          </div>
         </div>
       </div>
     </div>
